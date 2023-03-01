c1b62cd19f64c0017396ed4ce0dcff8ffba69f5e
Daniel <staubach.daniel@gmail.com>
2023-03-01 21:23:37 +0100

    added timing function

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import requests
 import urllib.request, json
+import time
 
 url = "https://lnkt4gjfcsr4rxvyr46icmbb5y0mgmuf.lambda-url.eu-central-1.on.aws/"
 
@@ -9,10 +10,13 @@ def healthcheck():
     if status_code != 200:
         print(f'Website not reachable: {status_code}')
         
-
 def getJsonContentFromUrl():
     with urllib.request.urlopen(url) as jsonRequest:
         jsonContent = json.load(jsonRequest)
         print(jsonContent)
 
-getJsonContentFromUrl()
+starttime = time.time()
+
+while True:
+    getJsonContentFromUrl()
+    time.sleep(5.0 - ((time.time() - starttime) % 5.0))
